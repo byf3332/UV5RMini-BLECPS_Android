7d89047215659272e54a1fcdb9ef0762e47e4cfb
byf3332 <71202267+byf3332@users.noreply.github.com>
2026-04-17 23:40:22 +0800

    Add BLE scan layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 27
         targetSdk = 36
         versionCode = 2
-        versionName = "1.0.2"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
